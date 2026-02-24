59b7af5291c6f8e977e1312f62fc45712995f7b8
Bhogaleaadi <92794343+Bhogaleaadi@users.noreply.github.com>
2026-02-24 15:37:13 +0530

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,7 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
   res.status(200).json({
     message: "Hello from Vercel Cloud!",
     cloud: "Vercel",
